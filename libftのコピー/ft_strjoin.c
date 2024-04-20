a52948722f913e083a3339c5f9a8123cace5d520
hfujita <c07014855@gmail.com>
2024-04-21 05:42:53 +0900

    strjoin

--- libftのコピー/ft_strjoin.c
+++ libftのコピー/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fujitaharuki <fujitaharuki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 06:26:37 by fujitaharuk       #+#    #+#             */
-/*   Updated: 2024/04/21 04:24:43 by fujitaharuk      ###   ########.fr       */
+/*   Updated: 2024/04/21 05:38:47 by fujitaharuk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,19 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	len_1;
-	size_t	len_2;
+	char	*start;
 	char	*dst;
 
 	if (!s1 || !s2)
 		return (NULL);
-	len_1 = ft_strlen(s1);
-	len_2 = ft_strlen(s2);
-	dst = malloc(len_1 + len_2 + 1);
+	dst = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!dst)
 		return (NULL);
-	ft_strlcpy(dst, s1, len_1 + 1);
-	ft_strlcat(dst, s2, len_1 + len_2 + 1);
-	return (dst);
+	start = dst;
+	while (*s1)
+		*dst++ = *s1++;
+	while (*s2)
+		*dst++ = *s2++;
+	*dst = '\0';
+	return (start);
 }
