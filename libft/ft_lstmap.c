117021b90b2ad5c6d46498160694c410903f8755
hfujita <c07014855@gmail.com>
2024-06-08 16:17:56 +0900

    lstmap

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,35 +6,51 @@
 /*   By: fujitaharuki <fujitaharuki@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 21:30:44 by fujitaharuk       #+#    #+#             */
-/*   Updated: 2024/04/27 06:15:07 by fujitaharuk      ###   ########.fr       */
+/*   Updated: 2024/06/07 10:59:13 by fujitaharuk      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+t_list	*do_1stmalloc(t_list *lst, t_list **map,
+		void *(*f)(void *), void (*del)(void *))
+{
+	void	*content;
+
+	content = f(lst->content);
+	*map = ft_lstnew(content);
+	if (!(*map))
+	{
+		del(content);
+		return (NULL);
+	}
+	return (*map);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*map;
 	t_list	*origin;
+	void	*content;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	map = ft_lstnew(f(lst->content));
-	if (map == NULL)
+	origin = do_1stmalloc(lst, &map, f, del);
+	if (!origin)
 		return (NULL);
-	origin = map;
 	lst = lst->next;
 	while (lst)
 	{
-		map->next = ft_lstnew(f(lst->content));
+		content = f(lst->content);
+		map->next = ft_lstnew(content);
 		if (map->next == NULL)
 		{
+			del(content);
 			ft_lstclear(&origin, del);
 			return (NULL);
 		}
 		map = map->next;
 		lst = lst->next;
 	}
-	map->next = NULL;
 	return (origin);
 }
